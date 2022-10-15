77af97eab60977fa349e70c5c4bd58fe7db78342
Everton Alves <everton10alves@gmail.com>
2022-10-15 16:26:55 -0300

    Tratando respostas com código de erro no client Java

--- src/main/java/com/algaworks/algafood/client/api/RestauranteClient.java
+++ src/main/java/com/algaworks/algafood/client/api/RestauranteClient.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import com.algaworks.algafood.client.model.RestauranteResumoModel;
@@ -13,18 +14,22 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class RestauranteClient {
 
-	private static String RESOURCE_PATH= "/restaurantes";
-	
+	private static String RESOURCE_PATH = "/restaurantes";
+
 	private RestTemplate restTemplate;
 	private String url = "localhost:8080";
-	
-	public List<RestauranteResumoModel> listar(){
-		URI  resourceUri = URI.create(url + RESOURCE_PATH);
-		
-		RestauranteResumoModel[] restaurantes = restTemplate
-				.getForObject(resourceUri, RestauranteResumoModel[].class);
-		
-		return Arrays.asList(restaurantes); 
+
+	public List<RestauranteResumoModel> listar() {
+		try {
+			URI resourceUri = URI.create(url + RESOURCE_PATH);
+
+			RestauranteResumoModel[] restaurantes = restTemplate.getForObject(resourceUri,
+					RestauranteResumoModel[].class);
+
+			return Arrays.asList(restaurantes);
+		} catch (RestClientResponseException e) {
+			throw new ClientApiException(e.getMessage(), e);
+		}
 	}
-	
+
 }
